a7f6a38d68ff24b3bea414146887f06bea4fadd8
Soufiane Abbad <soufiane.abbad@hotmail.com>
2020-11-06 17:07:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
-sudo apt-get -y update
-sudo apt-get -y upgrade
+sudo apt -y update
+sudo apt -y upgrade
 
 
-sudo apt-get install -y libcurl4-openssl-dev
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y jq
-sudo apt-get install -y ruby-full
-sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
-sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
-sudo apt-get install -y python-setuptools
-sudo apt-get install -y libldns-dev
-sudo apt-get install -y python3-pip
-sudo apt-get install -y python-pip
-sudo apt-get install -y python-dnspython
-sudo apt-get install -y git
-sudo apt-get install -y rename
-sudo apt-get install -y xargs
+sudo add-apt-repository universe
+
+sudo apt update
+
+sudo apt install -y libcurl4-openssl-dev
+sudo apt install -y libssl-dev
+sudo apt install -y jq
+sudo apt install -y ruby-full
+sudo apt install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+sudo apt install -y build-essential libssl-dev libffi-dev python-dev
+sudo apt install -y python-setuptools
+sudo apt install -y libldns-dev
+sudo apt install -y python3-pip
+sudo apt install -y python-pip
+sudo apt install -y python-dnspython
+sudo apt install -y git
+sudo apt install -y rename
+sudo apt install -y xargs
+sudo apt install -y gcc make libpcap-dev
+sudo apt install -y chromium-browser
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt -y install ./google-chrome-stable_current_amd64.deb
+
+rm https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+
+export VT_API_KEY=fa8d690606768aeddef7c0c363507fc7ccbc7947f3e38a82df79e6ba77c9f60d
+
 
 echo "installing bash_profile aliases from recon_profile"
-git clone https://github.com/nahamsec/recon_profile.git
+git clone https://github.com/SoufianeAbbad/recon_profile.git
 cd recon_profile
 cat .bash_profile >> ~/.bash_profile
 source ~/.bash_profile
-cd ~/tools/
 echo "done"
 
 
@@ -38,8 +54,8 @@ select choice in "${choices[@]}"; do
                 yes)
 
 					echo "Installing Golang"
-					wget https://dl.google.com/go/go1.13.10.linux-amd64.tar.gz
-					sudo tar -xvf go1.13.10.linux-amd64.tar.gz
+					wget https://dl.google.com/go/go1.15.4.linux-amd64.tar.gz
+					sudo tar -xvf go1.15.4.linux-amd64.tar.gz
 					sudo mv go /usr/local
 					export GOROOT=/usr/local/go
 					export GOPATH=$HOME/go
@@ -61,63 +77,13 @@ done
 fi
 
 
-#Don't forget to set up AWS credentials!
-echo "Don't forget to set up AWS credentials!"
-apt install -y awscli
-echo "Don't forget to set up AWS credentials!"
-
-
-
 #create a tools folder in ~/
 mkdir ~/tools
 cd ~/tools/
 
-#install aquatone
-echo "Installing Aquatone"
-go get github.com/michenriksen/aquatone
-echo "done"
-
-#install chromium
-echo "Installing Chromium"
-sudo snap install chromium
-echo "done"
-
-echo "installing JSParser"
-git clone https://github.com/nahamsec/JSParser.git
-cd JSParser*
-sudo python setup.py install
-cd ~/tools/
-echo "done"
-
-echo "installing Sublist3r"
-git clone https://github.com/aboul3la/Sublist3r.git
-cd Sublist3r*
-pip install -r requirements.txt
-cd ~/tools/
-echo "done"
-
-
-echo "installing teh_s3_bucketeers"
-git clone https://github.com/tomdev/teh_s3_bucketeers.git
-cd ~/tools/
-echo "done"
-
-
-echo "installing wpscan"
-git clone https://github.com/wpscanteam/wpscan.git
-cd wpscan*
-sudo gem install bundler && bundle install --without test
-cd ~/tools/
-echo "done"
 
 echo "installing dirsearch"
 git clone https://github.com/maurosoria/dirsearch.git
-cd ~/tools/
-echo "done"
-
-
-echo "installing lazys3"
-git clone https://github.com/nahamsec/lazys3.git
 cd ~/tools/
 echo "done"
 
@@ -126,30 +92,16 @@ git clone https://github.com/jobertabma/virtual-host-discovery.git
 cd ~/tools/
 echo "done"
 
-
-echo "installing sqlmap"
-git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-cd ~/tools/
-echo "done"
-
-echo "installing knock.py"
-git clone https://github.com/guelfoweb/knock.git
-cd ~/tools/
-echo "done"
-
-echo "installing lazyrecon"
-git clone https://github.com/nahamsec/lazyrecon.git
-cd ~/tools/
-echo "done"
-
 echo "installing nmap"
-sudo apt-get install -y nmap
+sudo apt install -y nmap
 echo "done"
 
 echo "installing massdns"
 git clone https://github.com/blechschmidt/massdns.git
 cd ~/tools/massdns
 make
+cd /bin 
+mv massdns /usr/bin
 cd ~/tools/
 echo "done"
 
@@ -160,33 +112,72 @@ pip install -r requirements.txt
 cd ~/tools/
 echo "done"
 
+echo "installing Arjun"
+git clone https://github.com/s0md3v/Arjun.git
+cd ~/tools/
+echo "done"
+
+echo "installing meg"
+go get -u github.com/tomnomnom/meg
+echo "done"
+
+echo "installing assetfinder"
+go get -u github.com/tomnomnom/assetfinder
+echo "done"
+
+echo "installing gau"
+GO111MODULE=on go get -u -v github.com/lc/gau
+echo "done"
+
 echo "installing httprobe"
-go get -u github.com/tomnomnom/httprobe 
+go get -u github.com/tomnomnom/httprobe
+echo "done"
+
+echo "installing qsreplace"
+go get -u github.com/tomnomnom/qsreplace
 echo "done"
 
 echo "installing unfurl"
-go get -u github.com/tomnomnom/unfurl 
+go get -u github.com/tomnomnom/unfurl
 echo "done"
 
 echo "installing waybackurls"
 go get github.com/tomnomnom/waybackurls
 echo "done"
 
-echo "installing crtndstry"
-git clone https://github.com/nahamsec/crtndstry.git
+echo "installing urinteresting"
+go get github.com/tomnomnom/hacks/urinteresting
 echo "done"
 
-echo "downloading Seclists"
-cd ~/tools/
-git clone https://github.com/danielmiessler/SecLists.git
-cd ~/tools/SecLists/Discovery/DNS/
-##THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
-cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
-cd ~/tools/
+echo "installing html-comments"
+go get -u github.com/tomnomnom/hacks/html-comments
+echo "done"
+
+echo "installing geteventlisteners"
+go get -u github.com/tomnomnom/hacks/geteventlisteners
+echo "done"
+
+echo "installing get-title"
+go get -u github.com/tomnomnom/hacks/get-title
+echo "done"
+
+echo "installing HTML-tool"
+go get -u github.com/tomnomnom/hacks/html-tool
+echo "done"
+
+echo "installing gowitness"
+go get -u github.com/sensepost/gowitness
+echo "done"
+
+echo "installing ffuf"
+go get -u github.com/ffuf/ffuf
+echo "done"
+
+echo "installing gau"
+GO111MODULE=on go get -u -v github.com/lc/gau
 echo "done"
 
 
 
 echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
 ls -la
-echo "One last time: don't forget to set up AWS credentials in ~/.aws/!"
